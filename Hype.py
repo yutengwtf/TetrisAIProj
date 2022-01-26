e574bd34c5afd3fdb56340d75590c107731a6472
yutengwtf <90594681+yutengwtf@users.noreply.github.com>
2022-01-26 12:23:31 +0800

    Update Hype.py

--- Hype.py
+++ Hype.py
@@ -11,20 +11,17 @@ RMSP_GRADIENT_MOMENTUM              = 0.95
 RMSP_SQUARED_GRADIENT_MOMENTUM      = 0.95
 RMSP_EPS                            = 0.01
 
-EPS_START1                          = 1
-EPS_END1                            = 0.618
-EPS_START2                          = 1
-EPS_END2                            = 0.382
-EPS_START3                          = 0.618
-EPS_END3                            = 0.382
-EPS_START4                          = 1
-EPS_END4                            = 0.1
+EPS_START                           = 1
+EPS_MID1                            = 0.618
+EPS_MID2                            = 0.382
+EPS_END                             = 0.1
+
+FRAME_START                         = 1
+FRAME_MID1                          = 16777216
+FRAME_MID2                          = 83886080
+FRAME_END                           = 100663296
 
 REPLAY_MEMORY_SIZE1                 = 131072
-
-START_FRAME                         = 1
-EPS_END_FRAME                       = 1048512
-END_FRAME                           = 1500000
 
 SAVE_FRAME                          = 50000
 REPLAY_START_SIZE                   = 65536
